031fdbbb424d99f6f4aa69a915df521df98c1c99
Huynh Van Tai <taiprogramer@protonmail.com>
2021-01-14 17:10:29 +0700

    [all customs are now in config.h]
    - Patches: [centeredmaster]

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *shutcmd[] = { "shutdown", "-h", "now", NULL };
+static const char *shutcmd[] = { "doas", "poweroff", "now", NULL };
 static const char *nnn[] = { "st", "--", "nnn", NULL };
 
 static Key keys[] = {
