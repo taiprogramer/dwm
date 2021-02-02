275f93052fcecdab9df6d0a7043aa3cad4157b34
Huynh Van Tai <taiprogramer@protonmail.com>
2021-02-02 16:06:46 +0700

    [use full path to poweroff for security]

--- config.h
+++ config.h
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_black, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *shutcmd[] = { "doas", "poweroff", "now", NULL };
+static const char *shutcmd[] = { "doas", "/usr/bin/poweroff", NULL };
 static const char *nnn[] = { "alacritty", "-e", "nnn", NULL };
 
 static Key keys[] = {
